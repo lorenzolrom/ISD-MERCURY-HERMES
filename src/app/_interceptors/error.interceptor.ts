fea5ee0f8986f3316e557536d7cec29fac6f78b1
Lorenzo L. Romero <lorenzolrom@gmail.com>
2020-05-17 15:19:59 -0400

    An API error will cause a visual error dialog to be thrown, and the GUI to redirect to the login page.

--- src/app/_interceptors/error.interceptor.ts
+++ src/app/_interceptors/error.interceptor.ts
@@ -1,25 +1,32 @@
 import { Injectable } from '@angular/core';
 import {HttpErrorResponse, HttpEvent, HttpHandler, HttpRequest} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
-import {catchError} from "rxjs/operators";
+import {catchError, retry} from "rxjs/operators";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorInterceptor {
 
-  constructor(private _snackBar: MatSnackBar) { }
-
-  handleError(error: HttpErrorResponse)
-  {
-    return throwError(error);
-  }
+  constructor(private _snackBar: MatSnackBar, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
   {
     return next.handle(req).pipe(
-      catchError(this.handleError)
+      retry(1),
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if(error.error instanceof ErrorEvent) {
+          errorMessage = `Error: ${error.error.error}`;
+        } else {
+          errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`
+        }
+        this._snackBar.open(errorMessage, 'Dismiss', {duration: 3000});
+        this.router.navigate(['/login']);
+        return throwError(errorMessage);
+      })
     )
   };
 }
